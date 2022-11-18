522e15efd9500c202e2256a1a081681c2a4048d0
vivomo <929992114@qq.com>
2022-11-18 23:27:47 +0800

    test: hello world

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+//
+// Created by Administrator on 2022/11/18 0018.
+//
+
+#include <stdio.h>
+
+int printHello() {
+    printf("Hello world\n");
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+#include "hello_world.c"
+
+int main() {
+    printHello();
+    return 0;
+}
