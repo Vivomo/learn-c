3380bf717cdd39821214bf0cb9a42c1fadaa25db
vivomo <929992114@qq.com>
2022-11-20 00:00:06 +0800

    test: print

---
+++ print.c
@@ -0,0 +1,12 @@
+//
+// Created by Administrator on 2022/11/18 0018.
+//
+#include <stdio.h>
+
+int main() {
+    double num = 3.4;
+    printf("print\n");
+    printf("%d%%\n", 3);
+    printf("%.2f%%\n", num);
+    return 0;
+}
