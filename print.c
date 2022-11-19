8c20aecb12dcc5dc4f9ce7770a119482e0241cca
vivomo <929992114@qq.com>
2022-11-20 00:01:12 +0800

    test: print

--- print.c
+++ print.c
@@ -2,11 +2,13 @@
 // Created by Administrator on 2022/11/18 0018.
 //
 #include <stdio.h>
+#define STR "str"
 
 int main() {
     double num = 3.4;
     printf("print\n");
     printf("%d%%\n", 3);
     printf("%.2f%%\n", num);
+    printf(STR);
     return 0;
 }
